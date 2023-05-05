3d929a4e1856da5055c0aa884745e107ccb73fc9
Monaliza Loren <83433815+monalizaloren@users.noreply.github.com>
2023-05-05 14:11:54 -0300

    Create js.js

---
+++ js.js
@@ -0,0 +1,6 @@
+handleTransaction = async () => {
+    var { bookId, studentId } = this.state;
+    /*Abaixo adicione as funções  getBookDetails e getStudentDetails para obter informações detalhadas sobre o livro e o estudante.
+     Essas funções fazem consultas no banco de dados para obter as informações desejadas.*/
+    ???
+    
